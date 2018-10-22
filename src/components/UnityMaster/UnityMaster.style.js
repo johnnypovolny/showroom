a39d3f0fa72b32bed9bfcf07ea647a0279706a93
Povolny, Johnny <Johnny.Povolny@nike.com>
2018-10-21 21:59:09 -0700

    Removing commented out style

--- src/components/UnityMaster/UnityMaster.style.js
+++ src/components/UnityMaster/UnityMaster.style.js
@@ -3,15 +3,12 @@ export const styleFunction = (props) => ({
   display: props.unity.isVisible && props.unityVisibleRoute ? 'block' : 'none',
   width: '100%',
   height: '100%',
-  // overflow: 'hidden',
   zIndex: 0,
   background: 'transparent',
   padding: '0 0 0 0',
   margin: '0 0 0 0',
-  '& canvas':{
+  '& canvas': {
     width: '100%',
     height: '100%'
-    // left: props.unity.isVisible && props.unityVisibleRoute ? '0%' : '100%'
   }
 });
-
